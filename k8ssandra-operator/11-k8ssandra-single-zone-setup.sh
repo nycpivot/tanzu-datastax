bc93d56f52cfa30819af2bf61cfb6fe25119b333
Michael James <mijames@vmware.com>
2022-07-28 15:04:32 -0400

    Replaced docker.io with Harbor proxy.

--- k8ssandra-operator/11-k8ssandra-single-zone-setup.sh
+++ k8ssandra-operator/11-k8ssandra-single-zone-setup.sh
@@ -4,31 +4,31 @@ rm k8ssandra-datacenter-single.yaml
 cat <<EOF | tee k8ssandra-datacenter-single.yaml
 cassandra:
   image: {}
-	  image:
-		  registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
+    image:
+      registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
   configBuilder:
-	  image:
-		  registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
+    image:
+      registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
   applyCustomConfig:
-	  image:
-		  registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
+    image:
+      registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
   jmxCredentialsConfig:
-	  image:
-		  registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
+    image:
+      registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
   loggingSidecar:
-	  image:
-		  registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
+    image:
+      registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
   waitForCassandra:
-	  image:
-		  registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
+    image:
+      registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
   image:
-	  registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
+    registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
  cleaner:
    image:
-	   registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
+     registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
  client:
    image:
-	   registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
+     registry: harbor-repo.vmware.com/dockerhub-proxy-cache/
   auth:
     superuser:
       secret: cassandra-admin-secret
